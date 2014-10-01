2683fc3837cd5989905bed522a227a3145c63b2d
dimalik <dim.alikaniotis@gmail.com>
2014-10-01 17:28:13 +0100

    added comments

--- reader.py
+++ reader.py
@@ -1,27 +1,55 @@
-import os
-import sys
-import re
-import logging
-import numpy as np
-from six import string_types
+"""
+Helper classes to load HiDEx (Shaoul & Westbury, 2006; 2010) and S-Space
+(https://github.com/fozziethebeat/S-Space) files into the Python interpreter.
 
-from gensim import corpora
-from scipy.stats.mstats import zscore
+Initialise a model with e.g.:
+>>> model = HidexModel(PATH_TO_DICT, PATH_TO_GCM)
+or
+>>> model = SSpaceModel(PATH_TO_SSPACE)
 
+PATH_TO_DICT should point to the .dict file output by HiDEx
+(e.g. combined.dict) and PATH_TO_GCM to the corresponding .gcm.
+In the case of S-Space, PATH_TO_SSPACE should point to the .sspace file which
+has to be saved using TEXT (i.e. not binary) format.
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+You can perform various word tasks with the model. Some of them
+are already built-in:
+
+>>> model.get_similarity('dog', 'cat') <- cosine similarity
+>>> model.get_neighbourhood('dog', topn=N) <- closest N neighbours using cosine
+    similarity
+>>> model.get_arc('dog') <- get neighbourhood size/density
+"""
 
 __author__ = 'Dimitrios Alikaniotis'
 __email__ = 'da352@cam.ac.uk'
 __affiliation__ = '\
                 University of Cambridge\
                 Department of Theoretical and Applied Linguistics'
-                
+
+import os
+import sys
+import re
+import logging
+
+import numpy as np
+from scipy.stats.mstats import zscore
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
+class GenericReader(object):
+    def __init__(self, file):
+        self.file = file
+        
+    def __iter__(self):
+        with open(self.file) as corpus:
+            for line in corpus:
+                yield line
         
 class ARCObject(dict):
     '''
-    customised dictionary object to hold also the name of the
-    word and have a nicer printing capability
+    read first the comments in the get_arc method in the model class
+    str(model.get_arc(word)) 
     '''
     def __init__(self, name, range_, *args, **kwargs):
         self.name = name
@@ -65,18 +93,10 @@ class Dictionary(dict):
 
 
 class Model(object):
-    '''
-    all the deriving models define just different ways
-    to load a vocabulary and a dictionary
-    needs a dictionary defining a token2id attribute
-    and a corresponding id2token
-    the __getitem__ method returns id2token
-    '''
-    
+        
     def _init(self):
         '''
-        normalise the matrix vector by vector (this can be quite expensive
-        for large matrices i.e HAL so search for alternative)
+        normalises each vector to unit length
         '''
         for i in range(self.model.shape[0]):
             if np.count_nonzero(self.model[i, :]) == 0:
@@ -90,7 +110,8 @@ class Model(object):
     def get_cosine(self, w1, w2):
         return np.dot(w1, w2)
         
-    def get_similarities(self, w1, w2):
+    def get_similarity(self, w1, w2):
+        #TODO: implement other similarity metrics
         try:
             words = [self.model[self.vocab.token2id[i]] for i in (w1,w2,)]
         except KeyError:
@@ -98,18 +119,36 @@ class Model(object):
         return self.get_cosine(*words)
         
     def get_neighbourhood(self, word, topn=10):
-        '''
-        returns a list of tuples containing the second word
-        and their cosine similarity
-        '''
         w_ind = self.vocab.token2id[word]
-        logging.info('Calculating the dot product of the V x D matrix and the word vector')
         dists = np.dot(self.model, self.model[w_ind])
         best = np.argsort(dists)[::-1]
         result = [(self.vocab[ind], float(dists[ind])) for ind in best if ind != w_ind]
-        return result[:topn] if topn else result
+        return result[:topn] if topn else result ## topn=0 shows all
         
     def get_arc(self, word):
+        '''
+        implements the neighbourhood size/density algorithm described in Alikaniotis (2014)
+        
+        the algorithm simply extends the proposal of Shaoul & Westbury (2006) by relativising
+        the threshold for EACH word. That is, we simply ask if the potential neighbour stands
+        closer to the target word or further away than the average pairing of the target with
+        any other word. In our implementation of the calculation of the semantic distances we
+        take A ⋅ w_i for each word (see get_neighbourhood method with topn=0), where A is the
+        ∣V∣ × D or the ∣V∣ × ∣V∣ vocabulary matrix and w_i is the target word. Having normalised
+        each vector in the matrix to unit length, (see the _init method) this operation is
+        equivalent to taking the cosine similarity between the word in question and all the
+        other words in the vocabulary.
+        The resulting ∣V∣ dimensional vector contains effectively the similarity values between
+        the word in question and all the other words in the matrix. From this point it is a
+        trivial task to obtain descriptive statistics for these distributions.
+        Converting, thus, the vector of similarities into z-scores, we are able to keep all
+        the scores above a predefined threshold obtaining thus neighbourhood size and density
+        for each word, taking into account its similarity to all the other words in the lexicon.
+        
+        The number of stdevs above which a word is considered a neighbour, does not have to be
+        explicitly set, the list_ variable reports the size and the density of the neighbourhood
+        in predefined steps.
+        '''
         list_ = np.arange(-5, 10, 1) ## range and step of reporting
         ans_list = ARCObject(word, list_)
         most_similar = self.get_neighbourhood(word, topn=0) ## get all
@@ -137,15 +176,6 @@ class Model(object):
             ans_ncount += 1
             ans_arc += k[1]
         return ans_list
-        
-class GenericReader(object):
-    def __init__(self, file):
-        self.file = file
-        
-    def __iter__(self):
-        with open(self.file) as corpus:
-            for line in corpus:
-                yield line
             
 class HidexModel(Model):
     def __init__(self, dict_file, gcm_file):
@@ -216,7 +246,7 @@ class SSpaceModel(Model):
 # PATH_TO_SSPACE = ''
 # hidex = HidexModel(PATH_TO_DICT, PATH_TO_GCM)
 # ## demo similarity
-# hidex.get_similarities('dog', 'cat')
+# hidex.get_similarity('dog', 'cat')
 # ## demo topN
 # hidex.get_neighbourhood('dog', topn=20)
 # ## demo arc
